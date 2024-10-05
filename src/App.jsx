a307e0e186a16a5720f7cacf61bfc443f525c32b
pawelsloboda5 <pawelsloboda5@gmail.com>
2024-10-05 02:13:51 -0400

    add vercel web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
